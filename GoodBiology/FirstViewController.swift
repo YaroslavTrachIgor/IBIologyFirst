8f2755b80699c454d0b72ecb335c757640fdd3b2
Ihor Trach <zhbr282@gmail.com>
2018-11-10 20:33:20 +0200

    Initial Commit

---
+++ GoodBiology/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GoodBiology
+//
+//  Created by Yaroslav on 11/10/18.
+//  Copyright © 2018 Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GoodBiology/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GoodBiology
+//
+//  Created by Yaroslav on 11/10/18.
+//  Copyright © 2018 Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
