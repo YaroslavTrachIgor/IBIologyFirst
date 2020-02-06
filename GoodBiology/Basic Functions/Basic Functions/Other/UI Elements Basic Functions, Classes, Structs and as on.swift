cadcc0e78dabdadf15b17fd28cfee641f8658e01
Yaroslav <zhbr282@gmail.com>
2020-02-06 23:22:18 +0200

    Test Commit. 8.0

---
+++ GoodBiology/Basic Functions/Basic Functions/Other/UI Elements Basic Functions, Classes, Structs and as on.swift
@@ -0,0 +1,27 @@
+//
+//  UI Basic Functions.swift
+//  GoodBiology
+//
+//  Created by Yaroslav Trach on 17.10.2019.
+//  Copyright © 2019 Yaroslav. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AudioToolbox
+
+import UIKit
+
+public func viewDidLoadPrinting(doing: String) {
+    print("View of \(doing) article was done.")
+}
+
+@available(iOS 13.0, *)
+class AppleButtonSettings {
+    var titleColor      = UIColor.label
+    var backgroundColor = UIColor.systemGray6
+    
+    var appleButtonCornerRadius = cornerRadius
+    
+    var font = UIFont(name: "HelveticaNeue-Bold", size: CGFloat(appleFontSize))
+}
