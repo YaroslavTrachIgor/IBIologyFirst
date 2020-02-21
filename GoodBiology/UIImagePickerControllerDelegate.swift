08b52366a93173a4c6b55d9924ffca4214bf4844
Yaroslav <zhbr282@gmail.com>
2020-02-21 12:53:11 +0200

    Commit. 15.0

---
+++ GoodBiology/UIImagePickerControllerDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  UIImagePickerControllerDelegate.swift
+//  GoodBiology
+//
+//  Created by Yaroslav Trach on 17.02.2020.
+//  Copyright © 2020 Yaroslav. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension SettingsTableViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        usersIconImageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
